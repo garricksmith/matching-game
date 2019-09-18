7d104b004fc6b5909a4a611eb1d7b8430dd1cea3
Garrick Smith <garrickdragosmith@gmail.com>
2019-09-17 20:57:50 -0400

    Fixed most known bugs in the javascript and added timer to game

--- web/scripts/script.js
+++ web/scripts/script.js
@@ -14,45 +14,59 @@ var incorrectMatches=0;
 function flip(image, seed, id, setn) {
     var card = document.getElementById(id);
     
-    if(failed == true){
-        var firstGuess = document.getElementById(firstGuessId);
-        var secondGuess = document.getElementById(secondGuessId);
-        firstGuess.innerHTML = "<img src=\"images/default.png\">";
-        secondGuess.innerHTML = "<img src=\"images/default.png\">";
-        failed = false;
-    }
-    
-    if(cardFlipped == false) {
-        cardFlipped = true;
-        firstGuessId = id;
-        firstGuessImage = image;
-        card.innerHTML = "<img src=\"images/"+setn[0]+"/"+setn[seed]+"\">";
-    } else {
-        totalAttemptedMatches++;
-        cardFlipped = false;
-        secondGuessId = id;
-        if(firstGuessImage == image) {
-            correctMatches++;
-            card.innerHTML = "<img src=\"images/"+setn[0]+"/"+setn[seed]+"\">";
+    if(firstGuessId != id) {
+        if(failed == true){
+            var firstGuess = document.getElementById(firstGuessId);
+            var secondGuess = document.getElementById(secondGuessId);
+            firstGuess.innerHTML = "<img src=\"images/default.png\">";
+            secondGuess.innerHTML = "<img src=\"images/default.png\">";
             failed = false;
-        } else {
-            incorrectMatches++;
-            card.innerHTML = "<img src=\"images/"+setn[0]+"/"+setn[seed]+"\">";
-            failed = true;
         }
-    }
-    
-    if(correctMatches == 10) {
-        var stats = document.getElementById("stats");
-        stats.innerHTML = "<h3>Correct Matches = "+correctMatches+"</h3>"+
-                          "<h3>Incorrect Matches = "+incorrectMatches+"</h3>"+
-                          "<h3>Total Attempted Matches = "+totalAttemptedMatches+"</h3>";
+        
+        if(cardFlipped == false) {
+            cardFlipped = true;
+            firstGuessId = id;
+            firstGuessImage = image;
+            card.innerHTML = "<img src=\"images/"+setn[0]+"/"+setn[seed]+"\">";
+        } else {
+            totalAttemptedMatches++;
+            cardFlipped = false;
+            secondGuessId = id;
+            if(firstGuessImage == image) {
+                correctMatches++;
+                card.innerHTML = "<img src=\"images/"+setn[0]+"/"+setn[seed]+"\">";
+                card.setAttribute("onclick", "");
+                failed = false;
+            } else {
+                incorrectMatches++;
+                card.innerHTML = "<img src=\"images/"+setn[0]+"/"+setn[seed]+"\">";
+                failed = true;
+            }
+        }
+        
+        if(totalAttemptedMatches == 1) {
+            startTimer(120);
+        }
+        
+        if(correctMatches == 10) {
+            alert("Congratulations! You Won! \nView Your Results Below");
+            var stats = document.getElementById("stats");
+            stats.innerHTML = "<h3>Correct Matches = "+correctMatches+"</h3>"+
+                              "<h3>Incorrect Matches = "+incorrectMatches+"</h3>"+
+                              "<h3>Total Attempted Matches = "+totalAttemptedMatches+"</h3>";
+        }
     }
 }
 
 function setBoard(setn) {
+    totalAttemptedMatches = 0;
+    correctMatches = 0;
+    incorrectMatches = 0;
+    cardFlipped = false;
+    
     var seeds = genSeeds();
     var board = document.getElementById("board");
+    var stats = document.getElementById("stats");
     var newBoard = "<tr>";
     
     var i;
@@ -60,14 +74,22 @@ function setBoard(setn) {
         if(i % 5 == 0 && i != 0) {
             newBoard = newBoard + "</tr><tr>";
         }
-        newBoard = newBoard + 
-            "<td id=\""+i+"\" onclick=flip(\""+setn[seeds[i]]+"\","+seeds[i]+","+i+",set1)>"+
-                "<img src=\"images/default.png\">"+
-            "</td>";
+        if(setn == set1){
+            newBoard = newBoard + 
+                "<td id=\""+i+"\" onclick=flip(\""+setn[seeds[i]]+"\","+seeds[i]+","+i+",set1)>"+
+                    "<img src=\"images/default.png\">"+
+                "</td>";
+        } else if(setn == set2){
+            newBoard = newBoard + 
+                "<td id=\""+i+"\" onclick=flip(\""+setn[seeds[i]]+"\","+seeds[i]+","+i+",set2)>"+
+                    "<img src=\"images/default.png\">"+
+                "</td>";
+        }
     }
     newBoard = newBoard + "</tr>";         
      
     board.innerHTML = newBoard;
+    stats.innerHTML = "";
 }
 
 function genSeeds() {
@@ -91,4 +113,26 @@ function genSeeds() {
     }
     
     return seeds;
+}
+
+function startTimer(duration) {
+    var timer = duration, minutes, seconds;
+    var stats = document.getElementById("stats");
+    
+    setInterval(function() {
+        minutes = parseInt(timer / 60,10);
+        seconds = parseInt(timer % 60,10);
+        
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;  
+        
+        if(--timer < 0) {
+        timer = 600;
+        alert("Time's Up! Stop playing and look at results");
+        stats.innerHTML = "<h3>Correct Matches = "+correctMatches+"</h3>"+
+                          "<h3>Incorrect Matches = "+incorrectMatches+"</h3>"+
+                          "<h3>Total Attempted Matches = "+totalAttemptedMatches+"</h3>";
+        }     
+    }, 1000);
+     
 }
